37c3021a00b6af4d22a0dcd7ec1043f218727e48
Ingwer Ludwig <ingwerludwig@MacBook-Air-M1.local>
2022-11-20 19:59:00 +0700

    feat: add user service as eureka client to eureka server

---
+++ microservices/src/main/java/org/BinarAcademy/microservices/UserServiceApplication.java
@@ -0,0 +1,21 @@
+package org.BinarAcademy.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import java.util.Collections;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(UserServiceApplication.class);
+		app.setDefaultProperties(Collections
+				.singletonMap("server.port", "8762"));
+		System.out.println("SERVER RUNNING ON PORT 8762 . . .");
+		app.run(args);
+	}
+
+}
